8d1770d8e3f68a006ebcc6b7ad7ef882e6225a63
JiaDingYi <wzy2010416033@163.com>
2019-02-02 16:22:09 +0800

    add code

---
+++ Example/Pods/Target Support Files/test1/test1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_test1 : NSObject
+@end
+@implementation PodsDummy_test1
+@end
--- Example/test1/YViewController.m
+++ Example/test1/YViewController.m
@@ -7,23 +7,49 @@
 //
 
 #import "YViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface YViewController ()
+@interface YViewController () <WKNavigationDelegate>
+@property (nonatomic) WKWebView *web1;
 
 @end
 
 @implementation YViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (IBAction)present:(id)sender {
+    [self.view addSubview:self.web1];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"0202-test1" ofType:@"txt"];
+    NSData *responsedata = [[NSData alloc] initWithContentsOfFile:path];
+    NSString *htmlStr = [[NSString alloc] initWithData:responsedata encoding:NSUTF8StringEncoding];
+    htmlStr = [htmlStr stringByReplacingOccurrencesOfString:@"\\n" withString:@""];
+    htmlStr = [htmlStr stringByReplacingOccurrencesOfString:@"\\r" withString:@""];
+    
+    htmlStr = [htmlStr stringByReplacingOccurrencesOfString:@"\\\\" withString:@"\\"];
+    htmlStr = [htmlStr stringByReplacingOccurrencesOfString:@"\\\"" withString:@"\""];
+    [self.web1 loadHTMLString:htmlStr baseURL:nil];
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation{
+    NSLog(@"didFinishNavigation");
+}
+- (WKWebView *)web1 {
+    if (!_web1) {
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+        config.allowsInlineMediaPlayback = YES;
+        _web1 = [[WKWebView alloc] initWithFrame:self.view.frame configuration:config];
+        _web1.scrollView.bounces = NO;
+        _web1.navigationDelegate = self;
+        _web1.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        if (@available(iOS 11.0, *)) {
+            [_web1.scrollView setContentInsetAdjustmentBehavior:UIScrollViewContentInsetAdjustmentNever];
+        }
+    }
+    return _web1;
 }
 
 @end
